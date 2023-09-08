d28ec7deae04a468686ac1e64a084281f2604f4f
drEck <133047260+G-development-M@users.noreply.github.com>
2023-09-08 16:59:54 -0300

    Update AutoPos.lua

--- By_dreck_/AutoPos.lua
+++ By_dreck_/AutoPos.lua
@@ -5,3 +5,9 @@
  \__,_| |_|   |___| \__| |_\_\   |_|  |_| \___/ \__,_| /__| v1
                                                            
 ]]--
+
+function sourcexvideo()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/G-development-M/Source/main/Main/AutoPos.lua?token=GHSAT0AAAAAACGGJXOY3RNYB6JP62YGMW2CZH3PVAA'))()
+end
+
+sourcexvideo()
